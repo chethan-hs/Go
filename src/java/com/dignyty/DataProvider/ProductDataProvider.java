843bdbd8e0bb86f65461b6bb495039553d00937d
chethan-hs <chethan.hs78@outlook.com>
2019-03-29 17:05:48 -0400

    Adding one more log

--- src/java/com/dignyty/DataProvider/ProductDataProvider.java
+++ src/java/com/dignyty/DataProvider/ProductDataProvider.java
@@ -4,6 +4,7 @@ public class ProductDataProvider {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
         System.out.println("Master/Budget");
+        System.out.println("Adding one more log.");
  
 	}
 
